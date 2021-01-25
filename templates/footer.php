2e8aea1c4f51e0ffcf4fb5f09645e59c5e8137f1
Selena Wells <selena.elise.wells@gmail.com>
2021-01-25 16:32:22 -0500

    initial structure added to index.php, modules have been created for php and scss

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer id="mainFooter">
+    <div class="footerWrap">
+        <p>Copyright &#xA9; <?php echo date('Y');?> London Referees Group</p>
+    </div>
+</footer>
---
+++ templates/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?></title>
+    <link rel="stylesheet" href="css/main.css">
+    <!--insert any google fonts here-->
+    <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+</head>
+
+<body>
