c59842a1ac08e6e530d16ead67e8be346dc57cad
Selena Wells <selena.elise.wells@gmail.com>
2021-01-28 14:43:13 -0500

    changes to index.php

--- index.php
+++ index.php
@@ -5,10 +5,48 @@ include  './templates/head.php'
 ?>
 
 <?php include './templates/header.php';?>
+<main class="mainContentWrap">
+    <section id="heroWrap">
+        <h2 class="hidden">Hero Image</h2>
+        <div class="heroTitle">Insert Catchy Phrase</div>
+        <div class="heroTagline">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</div>
+        <button class="button">Become A Referee</button>
+    </section>
 
+    <section id="whoWeAre">
+        <div class="banner">
+            <h2 class="bannerTitle">Who We Are</h2>
+        </div>
+        <div class="sectionContent">
+            <div class="sectionText">
+                <h3 class="tagline">A catchy sentance written here about referees</h3>
+                <p class="text">They've got a real good two-way game. Y'know Top shelf where Momma hides the cookies! We managed the puck well. That's an unfortunate play there. Turned the jets on. Extra effort. We wanna move the puck north south not east west.They've got a real good two-way game. Y'know Top shelf where Momma hides the cookies! We managed the puck well. That's an unfortunate play there. Turned the jets on. Extra effort. We wanna move the puck north south not east west.</p>
+            </div>
+            <div class="Image">image goes here</div>
+        </div>
+    </section>
 
+    <section id="ourServices">
+        <div class="banner">
+            <h2 class="bannerTitle">Our Services</h2>
+        </div>
+        <div class="sectionContent">
+            
+            
+        </div>
+    </section>
 
+    <section id="theReferee ">
+        <div class="banner">
+            <h2 class="bannerTitle">The Referee</h2>
+        </div>
+        <div class="sectionContent">
+            
+            
+        </div>
+    </section>
 
+</main>
 <?php include './templates/footer.php';?>
 
 <?php include './templates/foot.php';?>
