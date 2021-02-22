57f71939636630fdc257954cd9cf0e74de83af42
Selena Wells <selena.elise.wells@gmail.com>
2021-02-21 20:23:48 -0500

    rough header styles added

--- templates/header.php
+++ templates/header.php
@@ -3,24 +3,32 @@
         <h1 class="hidden">London Referees Group</h1>
         <div class="headerRibbonWrap">
              <ul class="headerRibbon">
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
-                <li class="partnersLogo">LOGO</li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
+                <li class="partnersLogo"><a href="#"><img src="./images/partners/logo.png" alt="partner logo"></a></li>
             </ul>
         </div>
-        <div class="mainLogo">LONDON REFEREES GROUP LOGO</div>
+        <div class="bottomNavWrap">      
         <nav class="navListWrap">
+            <div class="mainLogo">
+                <a href="index.html">
+                    <img src="./images/mainLogo" alt="">
+                </a>
+            </div>
             <ul class="mainNav">
                 <li class="navItem"><a href="#">Referees</a></li>
                 <li class="navItem"><a href="#">Membership</a></li>
                 <li class="navItem"><a href="#">Jr. Officials</a></li>
                 <li class="navItem"><a href="#">Hire Officials</a></li>
                 <li class="navItem"><a href="#">Contact</a></li>
+                <li class="navItem Login"><a href="#">Login</a></li>
             </ul>
-        </nav>    
+        </nav>   
+        </div>
+        
     </div>
 </header>
