c4f3b88b870da46f615ccb6d9b7f9884158bd747
Selena Wells <selena.elise.wells@gmail.com>
2021-03-26 00:38:34 -0400

    contact form styles added

--- contact.php
+++ contact.php
@@ -10,30 +10,39 @@ $title = "Contact";?>
             <div class="titleWrap"><h2 class="miniHeaderTitle">Contact</h2></div>
         </div>
         <h3 class="miniHeaderTagline">An orangization of more than 200 hockey referees proudly serving london and surrounding areas</h3>        
-    </section>
+</section>
+
+
+
+
 <section class="contactFormWrap">
-    <form action="./includes/mail/send.php" method="POST" id="mail-form">
-            <label for="firstname">Full Name</label>
-        <input type="text" id="name" name="name" placeholder="full name" required>
 
-        <label for="email">Email</label>
-        <input type="email" id="email" name="email" placeholder="email" required>
+    <form class="contactFormWrap" action="./includes/mail/send.php" method="POST" id="mail-form">
+    
+         <p class="contactFormParagraph">Are you interested in becoming a Referee, hire some of ours to your league or even just would like to know a bit more about who we are and what we do? You’ve come to the right place, then! Fill out the following contact form and ask away, we will get back to you as soon as 1 business day to set up an appointment.</p>
 
-        <label for="message">Your Message</label>
-        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+        <label for="firstname" class="hidden">Full Name</label>
+        <input type="text" id="name" name="name" placeholder="Name" required>
+
+        <label for="email" class="hidden">Email</label>
+        <input type="email" id="email" name="email" placeholder="Email" required>
+
+        <label for="message" class="hidden">Your Message</label>
+        <textarea name="message" id="message" cols="30" rows="10" placeholder="Message..."></textarea>
 
         <!-- <div class="g-recaptcha" data-sitekey="6Lcb7-cZAAAAAOA-4FcHohBjpcKcAvpAzDK68LDi"></div> -->
         <!--Automatically render the reCAPTCHA widget-->
 
-        <button class="submit-wrapper">
+        <button class="submit-wrapper button">
             <span class="submit">Send</span>
             
         </button>
 
-    </form>
+    </form>  
 
 
 </section>
+</div>
 
 
 <?php include './templates/footer.php';?>
